4b448ad6d97bb25d3b10cec5470b4ca635c03fa1
Jan943 <jansopata10@gmail.com>
2020-05-13 23:38:44 +0200

    Showing rectangles depending on periphery(big or small):
    - setting TextField in another GUI
    - displaying the rectangles which meet the criteria
    - using repository

---
+++ src/main/java/pl/sopata/rectanglesadder/RectangleSizeGui.java
@@ -0,0 +1,36 @@
+package pl.sopata.rectanglesadder;
+
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.Route;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Route
+public class RectangleSizeGui extends VerticalLayout{
+
+    private RectangleRepo rectangleRepo;
+
+    private TextField textFieldSize;
+    private TextArea textAreaRectangle;
+    private Button buttonBigger;
+    private Button buttonSmaller;
+
+    @Autowired
+    public RectangleSizeGui(RectangleRepo rectangleRepo) {
+        this.rectangleRepo = rectangleRepo;
+        this.textFieldSize = new TextField("Podaj rozmiar");
+        textAreaRectangle = new TextArea("Rezultat:");
+        buttonBigger = new Button("Pokaż większe");
+        buttonSmaller = new Button("Pokaż mniejsze");
+
+        buttonBigger.addClickListener(clickEvent -> textAreaRectangle.setValue(rectangleRepo.getBigRectangles(Integer.parseInt(textFieldSize.getValue())).toString()));
+        buttonSmaller.addClickListener(clickEvent -> textAreaRectangle.setValue(rectangleRepo.getSmallRectangles(Integer.parseInt(textFieldSize.getValue())).toString()));
+
+        add(textFieldSize);
+        add(buttonBigger);
+        add(buttonSmaller);
+        add(textAreaRectangle);
+    }
+}
